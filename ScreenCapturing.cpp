a63bda4437b19568aba43feca549e1a425a4a60a
Alexander Ismailov <tama.rilla@protonmail.com>
2023-04-18 13:47:30 +0200

    added linux sreen capturing support.
    Signed-off-by: Alexander Ismailov <tama.rilla@protonmail.com>

--- ScreenCapturing.cpp
+++ ScreenCapturing.cpp
@@ -1,6 +1,4 @@
-#if 0
-// g++ screena.cpp -o screena -lX11 -lXext -Ofast -mfpmath=both -march=native -m64 -funroll-loops -mavx2 `pkg-config opencv --cflags --libs` && ./screena
-
+#if BUILD_WITH_X11
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
@@ -8,103 +6,80 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
-#include <opencv2/opencv.hpp>  // This includes most headers!
-
-#include <time.h>
-#define FPS(start) (CLOCKS_PER_SEC / (clock()-start))
-
 struct ScreenShot
 {
-  ScreenShot(uint x,
-             uint y,
-             uint width,
-             uint height)
-    : x(x)
-    , y(y)
-    , width(width)
-    , height(height)
+  ScreenShot(cv::Rect rect)
+    : rect(rect)
   {
     display = XOpenDisplay(nullptr);
     root = DefaultRootWindow(display);
 
     XGetWindowAttributes(display, root, &window_attributes);
-    screen = window_attributes.screen;
-    ximg = XShmCreateImage(display, DefaultVisualOfScreen(screen), DefaultDepthOfScreen(screen), ZPixmap, NULL, &shminfo, width, height);
+    Screen* screen = window_attributes.screen;
+    ximg = XShmCreateImage(display, DefaultVisualOfScreen(screen), DefaultDepthOfScreen(screen), ZPixmap, NULL, &shminfo, rect.width, rect.height);
 
     shminfo.shmid = shmget(IPC_PRIVATE, ximg->bytes_per_line * ximg->height, IPC_CREAT|0777);
     shminfo.shmaddr = ximg->data = (char*)shmat(shminfo.shmid, 0, 0);
     shminfo.readOnly = False;
     if(shminfo.shmid < 0)
     {
-      puts("Fatal shminfo error!");
+      throw std::runtime_error("Fatal shminfo error!");
     }
     Status s1 = XShmAttach(display, &shminfo);
-    printf("XShmAttach() %s\n", s1 ? "success!" : "failure!");
-
-    init = true;
+    if (!s1)
+    {
+      throw std::runtime_error("XShmAttach() failure!");
+    }
   }
 
   void operator() (cv::Mat& cv_img)
   {
-    if (init)
-    {
-      init = false;
-    }
-
     XShmGetImage(display, root, ximg, 0, 0, 0x00ffffff);
-    cv_img = cv::Mat(height, width, CV_8UC4, ximg->data);
+    cv_img = cv::Mat(rect.height, rect.width, CV_8UC4, ximg->data);
+    cv::cvtColor(cv_img, cv_img, cv::COLOR_BGRA2GRAY);
   }
 
   ~ScreenShot()
   {
-    if (!init)
-    {
-      XDestroyImage(ximg);
-    }
-
+    XDestroyImage(ximg);
     XShmDetach(display, &shminfo);
     shmdt(shminfo.shmaddr);
     XCloseDisplay(display);
   }
 
-  Display* display;
+  Display* display{};
   Window root;
   XWindowAttributes window_attributes;
-  Screen* screen;
-  XImage* ximg;
+  XImage* ximg{};
   XShmSegmentInfo shminfo;
 
-  int x;
-  int y;
-  int width;
-  int height;
-  bool init;
+  cv::Rect rect;
 };
 
-
-int main()
-{
-  ScreenShot screen(0, 0, 1920, 1080);
-  cv::Mat img;
-
-  for(uint i;; ++i)
-  {
-    double start = clock();
-
-    screen(img);
-
-    if (!(i & 0b111111))
-    {
-      printf("fps %4.f  spf %.4f\n", FPS(start), 1 / FPS(start));
-    }
-    break;
-  }
-
-  cv::imshow("img", img);
-  cv::waitKey(0);
-}
+// g++ screena.cpp -o screena -lX11 -lXext -Ofast -mfpmath=both -march=native -m64 -funroll-loops -mavx2 `pkg-config opencv --cflags --libs` && ./screena
+//#define FPS(start) (CLOCKS_PER_SEC / (clock()-start))
+//int main()
+//{
+//  ScreenShot screen(0, 0, 1920, 1080);
+//  cv::Mat img;
+//
+//  for(uint i;; ++i)
+//  {
+//    double start = clock();
+//
+//    screen(img);
+//
+//    if (!(i & 0b111111))
+//    {
+//      printf("fps %4.f  spf %.4f\n", FPS(start), 1 / FPS(start));
+//    }
+//    break;
+//  }
+//
+//  cv::imshow("img", img);
+//  cv::waitKey(0);
+//}
 #endif
-
 
 #include "TimeMeasuring.hpp"
 #include "Header.hpp"
@@ -116,6 +91,7 @@ int main()
 #include <fstream>
 #include <map>
 
+#ifndef BUILD_WITH_X11
 #include <ApplicationServices/ApplicationServices.h>
 
 cv::Mat cvMatWithGrayImage(CGImageRef imageRef)
@@ -195,6 +171,16 @@ cv::Mat ScreenCapturingCropped(cv::Rect rect = {0, 0, 0, 0})
   }
   return cvMatWithGrayImage(screenShot);
 }
+#else
+cv::Mat ScreenCapturingCropped(cv::Rect rect = {0, 0, 1920, 1080})
+{
+  TAKEN_TIME();
+  static ScreenShot screen(rect);
+  cv::Mat img;
+  screen(img);
+  return img;
+}
+#endif
 
 struct DecodedObject
 {
