780784f79b81f17ec6cfd28fd025a7d5bfde4868
Alexander Ismailov <tama.rilla@protonmail.com>
2023-04-26 19:44:11 +0200

    Deleted installation of packets. Considering that it should be done manually.
    Signed-off-by: Alexander Ismailov <tama.rilla@protonmail.com>

--- build_host.sh
+++ build_host.sh
@@ -1,4 +1,4 @@
-sudo apt install -y libopencv-dev qtbase5-dev
+#sudo apt install -y libopencv-dev qtbase5-dev
 git submodule update --init --recursive
 cmake -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_WITH_OpenCV=ON -DBUILD_WITH_ZBar=ON
 cmake --build build
